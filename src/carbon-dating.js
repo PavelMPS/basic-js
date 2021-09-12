640514e35c85698ff71b14fa9040c023401ead13
Pavel <87997317+PavelMPS@users.noreply.github.com>
2021-09-12 20:55:05 +0300

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,6 +18,6 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  let t = Math.ceil((Math.log(MODERN_ACTIVITY/sampleActivity))/(Math.LN2/HALF_LIFE_PERIOD));
+  return t||false;
 }
