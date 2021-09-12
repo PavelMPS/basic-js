a91ee89979e39d4f72d207a9476f15c52719d21c
Pavel <87997317+PavelMPS@users.noreply.github.com>
2021-09-12 21:27:01 +0300

    Update count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -15,13 +15,12 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  */
 export default function countCats(matrix) {
-  let hiddenCats=0;
-  for(i=0;i<matrix.length;i++){
-    for(j=0;j<matrix[i].length;j++){
-      if(matrix[i][j]=='^^'){
-        hiddenCats++;
+  let hiddenCats = 0;
+  for (i = 0; i < matrix.length; i++) {
+      for (j = 0; j < matrix[i].length; j++) {
+          if (matrix[i][j] === '^^') {
+              hiddenCats++;
+          }
       }
-    }
-  }  
-  return hiddenCats;
+  } return hiddenCats;
 }
