7745fba6eb281bd87e771ea06fe2842bc6364b30
Pavel <87997317+PavelMPS@users.noreply.github.com>
2021-09-13 22:47:47 +0300

    feat: fix errors

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,20 +14,22 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default function transform(arr) {
-  let arr1 = arr;
-  let x;
-  for(let i = 0; i < arr.length; i++){
-     if(arr[i] === '--discard-next') {
-          x = arr1.splice(i,2);            
-     } else if(arr[i] == '--discard-prev') {
-          x = arr1.splice(i-1,2)
-     } else if(arr[i] == '--double-next') {
-          x = arr1.splice(i,1);
-          arr1.splice(i,0,arr[i]);
-     } else if(arr[i] == '--double-prev') {
-          x = arr1.splice(i,1);
-          arr1.splice(i-1,0,arr1[i-1]);
-     }
-  }
-  return arr1;
+     let arr1 = arr;
+     let x;
+     if (Array.isArray(arr)) {
+          for (let i = 0; i < arr.length; i++) {
+               if (arr[i] === '--discard-next') {
+                    x = arr1.splice(i, 2);
+               } else if (arr[i] == '--discard-prev') {
+                    x = arr1.splice(i - 1, 2)
+               } else if (arr[i] == '--double-next') {
+                    x = arr1.splice(i, 1);
+                    arr1.splice(i, 0, arr[i]);
+               } else if (arr[i] == '--double-prev') {
+                    x = arr1.splice(i, 1);
+                    arr1.splice(i - 1, 0, arr1[i - 1]);
+               }
+          }
+          return arr1;
+     } else return false;
 }
