3cfb4e2f789ee70a468cccb4497e77af50941e81
Pavel <87997317+PavelMPS@users.noreply.github.com>
2021-09-13 22:36:02 +0300

    feat: done transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,21 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  let arr1 = arr;
+  let x;
+  for(let i = 0; i < arr.length; i++){
+     if(arr[i] === '--discard-next') {
+          x = arr1.splice(i,2);            
+     } else if(arr[i] == '--discard-prev') {
+          x = arr1.splice(i-1,2)
+     } else if(arr[i] == '--double-next') {
+          x = arr1.splice(i,1);
+          arr1.splice(i,0,arr[i]);
+     } else if(arr[i] == '--double-prev') {
+          x = arr1.splice(i,1);
+          arr1.splice(i-1,0,arr1[i-1]);
+     }
+  }
+  return arr1;
 }
