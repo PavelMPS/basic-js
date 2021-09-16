05198101882c569a03dcc4bd399fbb958804d990
Pavel <87997317+PavelMPS@users.noreply.github.com>
2021-09-16 19:50:52 +0300

    add:chainmaker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,10 +1,10 @@
-
+import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Implement chainMaker object according to task description
  * 
  */
-const chainMaker = {
+export default chainMaker = {
   chain: '',
   getLength: function() {
     return (this.chain =='')? 0 : this.chain.split('~~').length;
@@ -33,3 +33,67 @@ const chainMaker = {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const chainMaker = {
+//   chain: '',
+//   getLength: function() {
+//     return (this.chain =='')? 0 : this.chain.split('~~').length;
+//   },
+//   addLink: function(value) {
+//    return (value!=undefined)? this.chain += `(${value})~~`:this.chain += `()~~`;
+//   },
+//   removeLink: function(position) {
+//     if(position>this.getLength()||position<this.getLength||position!=Number){
+//       alert(`You can't remove incorrect link!`)
+//     }else{
+//       let arr = this.chain.split('~~').splice(position, 1)
+//       this.chain = arr.join('~~');
+//     }    
+//   },
+//   reverseChain: function() {
+//     let arr = this.chain.split('~~').reverse();
+//     this.chain = arr.join('~~');
+//   },
+//   finishChain: function() {
+//     let fullChain = chain.slice(0, -2);
+//     this.chain = '';
+//     return fullChain;
+//   }
+// };
+
+
+
+// getLength() {
+//   throw new NotImplementedError('Not implemented');
+//   // remove line with error and write your code here
+// },
+// addLink(/* value */) {
+//   throw new NotImplementedError('Not implemented');
+//   // remove line with error and write your code here
+// },
+// removeLink(/* position */) {
+//   throw new NotImplementedError('Not implemented');
+//   // remove line with error and write your code here
+// },
+// reverseChain() {
+//   throw new NotImplementedError('Not implemented');
+//   // remove line with error and write your code here
+// },
+// finishChain() {
+//   throw new NotImplementedError('Not implemented');
+//   // remove line with error and write your code here
+// }
