16e3927dd1839e091b8faa1bf6fec790996c9fa0
Pavel <87997317+PavelMPS@users.noreply.github.com>
2021-09-16 19:34:20 +0300

    add:task simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,12 +4,13 @@ import { NotImplementedError } from '../extensions/index.js';
  * Implement chainMaker object according to task description
  * 
  */
-export default {
+const chainMaker = {
+  chain:'',
   getLength() {
-    return (chain =='')? 0 : chain.split('~~').length;
+    return (this.chain =='')? 0 : this.chain.split('~~').length;
   },
   addLink(value) {
-    (value!=undefined)? this.chain += `(${value})~~`:this.chain += `()~~`;
+   return (value!=undefined)? this.chain += `(${value})~~`:this.chain += `()~~`;
   },
   removeLink(position) {
     if(position>this.getLength()||position<this.getLength||position!=Number){
@@ -32,28 +33,3 @@ export default {
 
 
 
-// xport default {
-//   getLength(chain) {
-//     return (chain =='')? 0 : chain.split('~~').length;
-//   },
-//   addLink(value) {
-//     (value!=undefined)? this.chain += `(${value})~~`:this.chain += `()~~`;
-//   },
-//   removeLink(position) {
-//     if(position>this.getLength()||position<this.getLength||position!=Number){
-//       alert(`You can't remove incorrect link!`)
-//     }else{
-//       let arr = this.chain.split('~~').splice(position, 1)
-//       this.chain = arr.join('~~');
-//     }
-    
-//   },
-//   reverseChain() {
-//     let arr = this.chain.split('~~').reverse();
-//     this.chain = arr.join('~~');
-//   },
-//   finishChain() {
-//     let fullChain = chain.slice(0, -2);
-//     this.chain = '';
-//     return fullChain;
-//   }
