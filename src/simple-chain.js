4b7151e2316f3f2a477ca6d3c0fb15ba803bbd88
Pavel <87997317+PavelMPS@users.noreply.github.com>
2021-09-16 19:14:58 +0300

    add:task simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -6,23 +6,54 @@ import { NotImplementedError } from '../extensions/index.js';
  */
 export default {
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return (chain =='')? 0 : chain.split('~~').length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    (value!=undefined)? this.chain += `(${value})~~`:this.chain += `()~~`;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if(position>this.getLength()||position<this.getLength||position!=Number){
+      alert(`You can't remove incorrect link!`)
+    }else{
+      let arr = this.chain.split('~~').splice(position, 1)
+      this.chain = arr.join('~~');
+    }    
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let arr = this.chain.split('~~').reverse();
+    this.chain = arr.join('~~');
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let fullChain = chain.slice(0, -2);
+    this.chain = '';
+    return fullChain;
   }
 };
+
+
+
+// xport default {
+//   getLength(chain) {
+//     return (chain =='')? 0 : chain.split('~~').length;
+//   },
+//   addLink(value) {
+//     (value!=undefined)? this.chain += `(${value})~~`:this.chain += `()~~`;
+//   },
+//   removeLink(position) {
+//     if(position>this.getLength()||position<this.getLength||position!=Number){
+//       alert(`You can't remove incorrect link!`)
+//     }else{
+//       let arr = this.chain.split('~~').splice(position, 1)
+//       this.chain = arr.join('~~');
+//     }
+    
+//   },
+//   reverseChain() {
+//     let arr = this.chain.split('~~').reverse();
+//     this.chain = arr.join('~~');
+//   },
+//   finishChain() {
+//     let fullChain = chain.slice(0, -2);
+//     this.chain = '';
+//     return fullChain;
+//   }
